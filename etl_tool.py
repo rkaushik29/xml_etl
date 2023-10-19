cc4f998116da435cc77988300181cb313c9029de
r3kaushi <r3kaushi@uwaterloo.ca>
2023-10-19 17:11:00 -0400

    Classes and final test

--- etl_tool.py
+++ etl_tool.py
@@ -2,129 +2,133 @@ import xml.etree.ElementTree as Tree
 import unittest
 from etl_tool_test import ETLToolTest
 
-# Function to read an XML file and load the elements.
-# Since the presence of the file is tested here, we don"t test it elsewhere.
-def parse_xml(file_path):
-    try:
-        tree = Tree.parse(file_path)
-        return tree
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
 
-# Function to save the XML file after data manipulation.
-def save_xml(tree, save_path):
-    if tree is not None:
-        tree.write(save_path)
+class ETLTool:
+    # Constructor
+    def __init__(self):
+        self.tree = None
 
-# Modified price of items in a category.
-def modify_price(tree, category, increase):
-    root = tree.getroot()
-    for prod in root.findall(f"./product[@category='{category}']"):
-        price = float(prod.find("price").text)
-        new_price = price * (1 + increase/100)
-        prod.find("price").text = str(new_price)
+    # Function to read an XML file and load the elements.
+    # Since the presence of the file is tested here, we don"t test it elsewhere.
+    def parse_xml(self, file_path):
+        try:
+            self.tree = Tree.parse(file_path)
+        except Exception as e:
+            print(f"Error: {e}")
 
-# Rename a category to another one.
-def rename_category(tree, old_name, new_name):
-    root = tree.getroot()
-    for prod in root.findall(f"./product[@category='{old_name}']"):
-        prod.set("category", new_name)
+    # Function to save the XML file after data manipulation.
+    def save_xml(self, tree, save_path):
+        if tree is not None:
+            tree.write(save_path)
 
-# Removes products below a certain rating.
-def remove_products(tree, category, min_rating):
-    root = tree.getroot()
-    for prod in root.findall(f"./product[@category='{category}']"):
-        if float(prod.find("rating").text) < min_rating:
-            root.remove(prod)
+    # Modified price of items in a category.
+    def modify_price(self, tree, category, increase):
+        root = tree.getroot()
+        for prod in root.findall(f"./product[@category='{category}']"):
+            price = float(prod.find("price").text)
+            new_price = price * (1 + increase/100)
+            prod.find("price").text = str(new_price)
 
-# Outputs the report on the CLI
-def generate_report(tree):
-    report = {}
-    root = tree.getroot()
+    # Rename a category to another one.
+    def rename_category(self, tree, old_name, new_name):
+        root = tree.getroot()
+        for prod in root.findall(f"./product[@category='{old_name}']"):
+            prod.set("category", new_name)
 
-    for prod in root.findall("product"):
-        category = prod.attrib["category"]
-        price = float(prod.find("price").text)
-        report[category] = report.get(category, {"count": 0, "total_price": 0})
-        report[category]["count"] += 1
-        report[category]["total_price"] += price
+    # Removes products below a certain rating.
+    def remove_products(self, tree, category, min_rating):
+        root = tree.getroot()
+        for prod in root.findall(f"./product[@category='{category}']"):
+            if float(prod.find("rating").text) < min_rating:
+                root.remove(prod)
 
-    for k, v in report.items():
-        print(f"\nCategory: {k}")
-        print(f"Total Product Count: {v['count']}")
-        print(f"Total Price: {v['total_price']}")
+    # Outputs the report on the CLI
+    def generate_report(self, tree):
+        report = {}
+        root = tree.getroot()
 
-# Runs the UI for the tool as a menu based interface.
-def run_tool():
-    while True:
-        print("\nWelcome to the Product ETL Tool")
-        print("1: Load XML File")
-        print("2: Increase Price By Percent")
-        print("3: Rename Category")
-        print("4: Remove Products Below Minimum Rating")
-        print("5: Save Changes to New File")
-        print("6: Generate Report on CLI")
-        print("7: Run Unit Test Suite")
-        print("8: Exit")
+        for prod in root.findall("product"):
+            category = prod.attrib["category"]
+            price = float(prod.find("price").text)
+            report[category] = report.get(category, {"count": 0, "total_price": 0})
+            report[category]["count"] += 1
+            report[category]["total_price"] += price
 
-        select = input("Enter your choice here (Number): ")
-        LOAD_ERR = "Please load an XML file first by selecting option 1."
+        for k, v in report.items():
+            print(f"\nCategory: {k}")
+            print(f"Total Product Count: {v['count']}")
+            print(f"Total Price: {v['total_price']}")
 
-        # Menu based CLI
-        if select == "1":
-            file_path = input("Enter the relative path to the XML file: ")
-            tree = parse_xml(file_path)
-        
-        elif select == "2":
-            # Check if 'tree' is a variable in the scope. If not, we first need to load it (1)
-            if "tree" not in locals():
-                print(LOAD_ERR)
-                continue
-            category = input("Enter the category name: ")
-            percentage = float(input("Enter the percentage increase (number only): "))
-            modify_price(tree, category, percentage)
-        
-        elif select == "3":
-            if "tree" not in locals():
-                print(LOAD_ERR)
-                continue
-            old_name = input("Enter the current category name: ")
-            new_name = input("Enter the new category name: ")
-            rename_category(tree, old_name, new_name)
-        
-        elif select == "4":
-            if "tree" not in locals():
-                print(LOAD_ERR)
-                continue
-            category = input("Enter the category name: ")
-            min_rating = float(input("Enter the minimum rating: "))
-            remove_products(tree, category, min_rating)
-        
-        elif select == "5":
-            if "tree" not in locals():
-                print(LOAD_ERR)
-                continue
-            save_path = input("Enter the path to save the XML file: ")
-            save_xml(tree, save_path)
-        
-        elif select == "6":
-            if "tree" not in locals():
-                print(LOAD_ERR)
-                continue
-            generate_report(tree)
+    # Runs the UI for the tool as a menu based interface.
+    def run_tool(self):
+        while True:
+            print("\nWelcome to the Product ETL Tool")
+            print("1: Load XML File")
+            print("2: Increase Price By Percent")
+            print("3: Rename Category")
+            print("4: Remove Products Below Minimum Rating")
+            print("5: Save Changes to New File")
+            print("6: Generate Report on CLI")
+            print("7: Run Unit Test Suite")
+            print("8: Exit")
 
-        elif select == "7":
-            test = unittest.TestLoader().loadTestsFromTestCase(ETLToolTest)
-            if test:
-                print("\nRunning Unit Test Suite...")
-                unittest.TextTestRunner().run(test=test)
-            else:
-                print("Test Case unavailable")
+            select = input("Enter your choice here (Number): ")
+            LOAD_ERR = "Please load an XML file first by selecting option 1."
 
-        elif select == "8":
-            break
+            # Menu based CLI
+            if select == "1":
+                file_path = input("Enter the relative path to the XML file: ")
+                self.parse_xml(file_path)
+            
+            elif select == "2":
+                # Check if tree is filled with a loaded XML. If not, we first need to load it (1)
+                if self.tree is not None:
+                    print(LOAD_ERR)
+                    continue
+                category = input("Enter the category name: ")
+                percentage = float(input("Enter the percentage increase (number only): "))
+                self.modify_price(self.tree, category, percentage)
+            
+            elif select == "3":
+                if self.tree is not None:
+                    print(LOAD_ERR)
+                    continue
+                old_name = input("Enter the current category name: ")
+                new_name = input("Enter the new category name: ")
+                self.rename_category(self.tree, old_name, new_name)
+            
+            elif select == "4":
+                if self.tree is not None:
+                    print(LOAD_ERR)
+                    continue
+                category = input("Enter the category name: ")
+                min_rating = float(input("Enter the minimum rating: "))
+                self.remove_products(self.tree, category, min_rating)
+            
+            elif select == "5":
+                if self.tree is not None:
+                    print(LOAD_ERR)
+                    continue
+                save_path = input("Enter the path to save the XML file: ")
+                self.save_xml(self.tree, save_path)
+            
+            elif select == "6":
+                if self.tree is not None:
+                    print(LOAD_ERR)
+                    continue
+                self.generate_report(self.tree)
+
+            elif select == "7":
+                test = unittest.TestLoader().loadTestsFromTestCase(ETLToolTest)
+                if test:
+                    print("\nRunning Unit Test Suite...")
+                    unittest.TextTestRunner().run(test=test)
+                else:
+                    print("Test Case unavailable")
+
+            elif select == "8":
+                break
 
 
 if __name__ == "__main__":
-    run_tool()
+    etl_tool = ETLTool()
--- etl_tool_test.py
+++ etl_tool_test.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 import xml.etree.ElementTree as Tree
 from etl_tool import parse_xml, modify_price, rename_category, remove_products, generate_report
 
@@ -6,20 +7,66 @@ from etl_tool import parse_xml, modify_price, rename_category, remove_products, 
 class ETLToolTest(unittest.TestCase):
     # Set up the test case with any sample data
     def setUp(self):
-        self.sample_data = '''
-                            <products>
-                                <product category="Electronics">
-                                <name>Iphone 12 Pro</name>
-                                <price>599.99</price>
-                                <rating>4.5</rating>
-                                </product>
-                                <product category="Books">
-                                <name>Python for Beginners</name>
-                                <price>29.99</price>
-                                <rating>4.0</rating>
-                                </product>
-                            </products>
-                            '''
+        self.sample_data = (
+        '''
+        <products>
+            <product category="Electronics">
+            <name>Iphone 12 Pro</name>
+            <price>599.99</price>
+            <rating>4.5</rating>
+            </product>
+            <product category="Books">
+            <name>Python for Beginners</name>
+            <price>29.99</price>
+            <rating>4.0</rating>
+            </product>
+        </products>
+        '''
+        )
+        # Generate a element tree from test data
         self.tree = Tree.ElementTree(Tree.fromstring(self.sample_data))
     
+    # Testing the parsing and loading of the XML doc
+    def test_parse_xml(self):
+        new_tree = parse_xml("test.xml")
+        root = new_tree.getroot()
+        self.assertEqual(root.tag, "products")      # assertion changes with change in test data
+
+    def test_save_changes(self):
+        # Save to a temporary file and then read to verify
+        out_file = "temp.xml"
+        self.tree.write(out_file)
+        tree = Tree.parse(out_file)
+        root = tree.getroot()
+        self.assertEqual(root.tag, 'products')
+        os.remove(out_file)  # Cleanup temp file
+
+    # Testing modification of price by imitating actual call
+    def test_modify_price(self):
+        category, increase_pct = "Electronics", 10
+        modify_price(self.tree, category, increase_pct)
+        for product in self.tree.getroot().findall("./product[@category='Electronics']"):
+            self.assertEqual(float(product.find('price').text), 659.989)
     
+    # Testing rename of category
+    def test_rename(self):
+        old_name, new_name = "Books", "Python Books"
+        rename_category(self.tree, old_name, new_name)
+        for product in self.tree.getroot().findall("./product[@category='Python Books']"):
+            self.assertIsNotNone(product)
+    
+    # Testing removal of products below a min rating
+    def test_remove_products(self):
+        category, min_rating = "Books", 4.2
+        remove_products(self.tree, category, min_rating)
+        for product in self.tree.getroot().findall("./product[@category='Books']"):
+            self.assertIsNone(product)
+    
+    def test_generate_report(self):
+
+        report = generate_report(self.tree)
+        self.assertEqual(report, {'Electronics': {'count': 1, 'total_price': 599.99},
+                                  'Books': {'count': 1, 'total_price': 29.99}})
+
+if __name__ == "__main__":
+    unittest.main()
