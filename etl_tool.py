86dc62717fb21c92b55c5a3d8404698cce0e8a0e
r3kaushi <r3kaushi@uwaterloo.ca>
2023-10-19 16:44:44 -0400

    cli menu ui

--- etl_tool.py
+++ etl_tool.py
@@ -1,7 +1,9 @@
 import xml.etree.ElementTree as Tree
+import unittest
+from etl_tool_test import ETLToolTest
 
 # Function to read an XML file and load the elements.
-# Since the presence of the file is tested here, we don't test it elsewhere.
+# Since the presence of the file is tested here, we don"t test it elsewhere.
 def parse_xml(file_path):
     try:
         tree = Tree.parse(file_path)
@@ -41,7 +43,7 @@ def generate_report(tree):
     report = {}
     root = tree.getroot()
 
-    for prod in root.findall('product'):
+    for prod in root.findall("product"):
         category = prod.attrib["category"]
         price = float(prod.find("price").text)
         report[category] = report.get(category, {"count": 0, "total_price": 0})
@@ -52,3 +54,77 @@ def generate_report(tree):
         print(f"\nCategory: {k}")
         print(f"Total Product Count: {v['count']}")
         print(f"Total Price: {v['total_price']}")
+
+# Runs the UI for the tool as a menu based interface.
+def run_tool():
+    while True:
+        print("\nWelcome to the Product ETL Tool")
+        print("1: Load XML File")
+        print("2: Increase Price By Percent")
+        print("3: Rename Category")
+        print("4: Remove Products Below Minimum Rating")
+        print("5: Save Changes to New File")
+        print("6: Generate Report on CLI")
+        print("7: Run Unit Test Suite")
+        print("8: Exit")
+
+        select = input("Enter your choice here (Number): ")
+        LOAD_ERR = "Please load an XML file first by selecting option 1."
+
+        # Menu based CLI
+        if select == "1":
+            file_path = input("Enter the relative path to the XML file: ")
+            tree = parse_xml(file_path)
+        
+        elif select == "2":
+            # Check if 'tree' is a variable in the scope. If not, we first need to load it (1)
+            if "tree" not in locals():
+                print(LOAD_ERR)
+                continue
+            category = input("Enter the category name: ")
+            percentage = float(input("Enter the percentage increase (number only): "))
+            modify_price(tree, category, percentage)
+        
+        elif select == "3":
+            if "tree" not in locals():
+                print(LOAD_ERR)
+                continue
+            old_name = input("Enter the current category name: ")
+            new_name = input("Enter the new category name: ")
+            rename_category(tree, old_name, new_name)
+        
+        elif select == "4":
+            if "tree" not in locals():
+                print(LOAD_ERR)
+                continue
+            category = input("Enter the category name: ")
+            min_rating = float(input("Enter the minimum rating: "))
+            remove_products(tree, category, min_rating)
+        
+        elif select == "5":
+            if "tree" not in locals():
+                print(LOAD_ERR)
+                continue
+            save_path = input("Enter the path to save the XML file: ")
+            save_xml(tree, save_path)
+        
+        elif select == "6":
+            if "tree" not in locals():
+                print(LOAD_ERR)
+                continue
+            generate_report(tree)
+
+        elif select == "7":
+            test = unittest.TestLoader().loadTestsFromTestCase(ETLToolTest)
+            if test:
+                print("\nRunning Unit Test Suite...")
+                unittest.TextTestRunner().run(test=test)
+            else:
+                print("Test Case unavailable")
+
+        elif select == "8":
+            break
+
+
+if __name__ == "__main__":
+    run_tool()
